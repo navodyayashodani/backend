cee12d0ce939eab754cd1f5172333a6bb90e0373
Navodya Yashodani <nayashodani567@gmail.com>
2026-03-25 11:04:55 +0530

    update settings

---
+++ accounts/management/commands/create_superuser_if_needed.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates a superuser if one does not exist'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@example.com", "admin123")
+            self.stdout.write("Superuser created.")
+        else:
+            self.stdout.write("Superuser already exists.")
--- backend/settings.py
+++ backend/settings.py
@@ -174,10 +174,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-import os
-from django.contrib.auth import get_user_model
-
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
